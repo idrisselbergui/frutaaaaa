8c55c624cef368d68700cf136c6274145fb6e53e
info <info@DESKTOP-P027BJ8>
2025-12-05 14:40:18 +0100

    before addingnumlot on vente  tell no everything working good

--- frutaaaaa/Controllers/VenteEcartController.cs
+++ frutaaaaa/Controllers/VenteEcartController.cs
@@ -42,9 +42,9 @@ namespace frutaaaaa.Controllers
             {
                 using (var _context = CreateDbContext(database))
                 {
-                    var query = _context.EcartDirects
+                    IQueryable<EcartDirect> query = _context.EcartDirects
                         .Include(e => e.TypeEcart)
-                        .Where(e => e.Codtype == codtype && e.Numvent == null);
+                        .Where(e => e.Numvent == null && e.Codtype == codtype);
 
                     if (startDate.HasValue)
                     {
@@ -73,21 +73,23 @@ namespace frutaaaaa.Controllers
             {
                 using (var _context = CreateDbContext(database))
                 {
-                    var query = _context.EcartEs
+                    IQueryable<EcartE> query = _context.EcartEs
                         .Join(_context.EcartDs, e => e.numpal, d => d.numpal, (e, d) => new { e, d })
-                        .Where(x => x.d.codtype == codtype && x.e.numvent == null);
+                        .Where(x => x.e.numvent == null && x.d.codtype == codtype)
+                        .Select(x => x.e)
+                        .Distinct();
 
                     if (startDate.HasValue)
                     {
-                        query = query.Where(x => x.e.dtepal >= startDate.Value.Date);
+                        query = query.Where(e => e.dtepal >= startDate.Value.Date);
                     }
                     if (endDate.HasValue)
                     {
                         var endDateInclusive = endDate.Value.Date.AddDays(1).AddTicks(-1);
-                        query = query.Where(x => x.e.dtepal <= endDateInclusive);
+                        query = query.Where(e => e.dtepal <= endDateInclusive);
                     }
 
-                    return await query.Select(x => x.e).Distinct().ToListAsync();
+                    return await query.ToListAsync();
                 }
             }
             catch (Exception ex)
@@ -137,6 +139,8 @@ namespace frutaaaaa.Controllers
                             _context.Ventes.Add(vente);
                             await _context.SaveChangesAsync();
 
+                            Console.WriteLine($"Created Vente with ID: {vente.Id}");
+
                             // Update selected ecarts
                             foreach (var selectedEcart in request.SelectedEcarts)
                             {
@@ -145,9 +149,14 @@ namespace frutaaaaa.Controllers
                                     var ecart = await _context.EcartDirects.FindAsync(selectedEcart.Id);
                                     if (ecart != null)
                                     {
+                                        Console.WriteLine($"Found EcartDirect with ID: {selectedEcart.Id}, associating with Vente ID: {vente.Id}");
                                         ecart.Numvent = vente.Id;
                                         ecart.Pdsvent = selectedEcart.Pdsvent;
                                         _context.Entry(ecart).State = EntityState.Modified;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine($"EcartDirect with ID: {selectedEcart.Id} not found.");
                                     }
                                 }
                                 else if (selectedEcart.Table == "ecart_e")
@@ -155,9 +164,14 @@ namespace frutaaaaa.Controllers
                                     var ecart = await _context.EcartEs.FindAsync(selectedEcart.Id);
                                     if (ecart != null)
                                     {
+                                        Console.WriteLine($"Found EcartE with ID: {selectedEcart.Id}, associating with Vente ID: {vente.Id}");
                                         ecart.numvent = vente.Id;
                                         ecart.pdsvent = selectedEcart.Pdsvent;
                                         _context.Entry(ecart).State = EntityState.Modified;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine($"EcartE with ID: {selectedEcart.Id} not found.");
                                     }
                                 }
                             }
@@ -180,9 +194,9 @@ namespace frutaaaaa.Controllers
             }
         }
 
-        // PUT: api/vente-ecart/{id}
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateVente([FromHeader(Name = "X-Database-Name")] string database, int id, [FromBody] VenteUpdateRequest request)
+        // GET: api/vente-ecart/{id}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetVente([FromHeader(Name = "X-Database-Name")] string database, int id)
         {
             try
             {
@@ -191,14 +205,131 @@ namespace frutaaaaa.Controllers
                     var vente = await _context.Ventes.FindAsync(id);
                     if (vente == null) return NotFound();
 
-                    vente.Numbonvente = request.Numbonvente ?? vente.Numbonvente;
-                    vente.Date = request.Date ?? vente.Date;
-                    vente.Price = request.Price ?? vente.Price;
-                    vente.PoidsTotal = request.PoidsTotal ?? vente.PoidsTotal;
-                    vente.MontantTotal = request.MontantTotal ?? vente.MontantTotal;
+                    var directEcarts = await _context.EcartDirects
+                        .Where(e => e.Numvent == id)
+                        .Select(e => new {
+                            table = "ecart_direct",
+                            id = e.Numpal,
+                            pdsvent = e.Pdsvent,
+                            refver = e.Refver,
+                            codvar = e.Codvar
+                        })
+                        .ToListAsync();
 
-                    await _context.SaveChangesAsync();
-                    return Ok(new { message = "Vente updated successfully" });
+                    var eEcarts = await _context.EcartEs
+                        .Where(e => e.numvent == id)
+                        .Select(e => new {
+                            table = "ecart_e",
+                            id = e.numpal,
+                            pdsvent = e.pdsvent,
+                            refver = e.refver,
+                            codvar = e.codvar
+                        })
+                        .ToListAsync();
+
+                    var ecarts = directEcarts.Cast<object>().Concat(eEcarts.Cast<object>()).ToList();
+
+                    Console.WriteLine($"Found {ecarts.Count} ecarts for Vente ID: {id}");
+
+                    var codtype = 0;
+                    var directType = await _context.EcartDirects.Where(e => e.Numvent == id).Select(e => e.Codtype).FirstOrDefaultAsync();
+                    if (directType != null && directType != 0)
+                    {
+                        codtype = directType.Value;
+                    }
+                    else
+                    {
+                        var eType = await _context.EcartEs
+                            .Join(_context.EcartDs, e => e.numpal, d => d.numpal, (e, d) => new { e, d })
+                            .Where(x => x.e.numvent == id)
+                            .Select(x => x.d.codtype)
+                            .FirstOrDefaultAsync();
+                        codtype = eType;
+                    }
+
+                    return Ok(new { vente, ecarts, codtype });
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
+
+        // PUT: api/vente-ecart/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateVenteEcart([FromHeader(Name = "X-Database-Name")] string database, int id, [FromBody] VenteEcartRequest request)
+        {
+            try
+            {
+                using (var _context = CreateDbContext(database))
+                {
+                    using (var transaction = await _context.Database.BeginTransactionAsync())
+                    {
+                        try
+                        {
+                            var vente = await _context.Ventes.FindAsync(id);
+                            if (vente == null) return NotFound();
+
+                            // Update Vente record
+                            vente.Numbonvente = request.Numbonvente ?? vente.Numbonvente;
+                            vente.Date = request.Date;
+                            vente.Price = request.Price;
+                            vente.PoidsTotal = request.PoidsTotal;
+                            vente.MontantTotal = request.MontantTotal;
+
+                            // Reset old ecarts
+                            var directEcarts = await _context.EcartDirects.Where(e => e.Numvent == id).ToListAsync();
+                            foreach (var ecart in directEcarts)
+                            {
+                                ecart.Numvent = null;
+                                ecart.Pdsvent = null;
+                                _context.Entry(ecart).State = EntityState.Modified;
+                            }
+                            var eEcarts = await _context.EcartEs.Where(e => e.numvent == id).ToListAsync();
+                            foreach (var ecart in eEcarts)
+                            {
+                                ecart.numvent = null;
+                                ecart.pdsvent = null;
+                                _context.Entry(ecart).State = EntityState.Modified;
+                            }
+                            await _context.SaveChangesAsync();
+
+                            // Set new selected ecarts
+                            foreach (var selectedEcart in request.SelectedEcarts)
+                            {
+                                if (selectedEcart.Table == "ecart_direct")
+                                {
+                                    var ecart = await _context.EcartDirects.FindAsync(selectedEcart.Id);
+                                    if (ecart != null && ecart.Numvent == null) // Check not taken
+                                    {
+                                        ecart.Numvent = vente.Id;
+                                        ecart.Pdsvent = selectedEcart.Pdsvent;
+                                        _context.Entry(ecart).State = EntityState.Modified;
+                                    }
+                                }
+                                else if (selectedEcart.Table == "ecart_e")
+                                {
+                                    var ecart = await _context.EcartEs.FindAsync(selectedEcart.Id);
+                                    if (ecart != null && ecart.numvent == null) // Check not taken
+                                    {
+                                        ecart.numvent = vente.Id;
+                                        ecart.pdsvent = selectedEcart.Pdsvent;
+                                        _context.Entry(ecart).State = EntityState.Modified;
+                                    }
+                                }
+                            }
+                            await _context.SaveChangesAsync();
+                            await transaction.CommitAsync();
+
+                            return Ok(new { message = "Vente updated successfully" });
+                        }
+                        catch (Exception ex)
+                        {
+                            await transaction.RollbackAsync();
+                            throw ex;
+                        }
+                    }
                 }
             }
             catch (Exception ex)
@@ -275,14 +406,5 @@ namespace frutaaaaa.Controllers
         public string Table { get; set; } // "ecart_direct" or "ecart_e"
         public int Id { get; set; } // Numpal for direct, numpal for E
         public double? Pdsvent { get; set; }
-    }
-
-    public class VenteUpdateRequest
-    {
-        public int? Numbonvente { get; set; }
-        public DateTime? Date { get; set; }
-        public double? Price { get; set; }
-        public double? PoidsTotal { get; set; }
-        public double? MontantTotal { get; set; }
     }
 }
